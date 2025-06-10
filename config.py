91ba1b0d742a167a0da1a6fbcd8e36807f7f142f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-06-10 17:33:15 +0100

    Update config.py

--- config.py
+++ config.py
@@ -20,17 +20,5 @@ class Config:
         self.data_ = os.path.join(os.getcwd(), 'data')
         self.warehouse = os.path.join(os.getcwd(), 'warehouse')
 
-        self.variational_ = os.path.join(self.warehouse, 'variational')
-        self.points_ = os.path.join(self.variational_, 'points')
-        self.menu_ = os.path.join(self.variational_, 'menu')
-
-        # The model assets section
-        self.origin_ = 'assets/variational/{stamp}'
-
         # Keys, etc
         self.s3_parameters_key = 's3_parameters.yaml'
-        self.argument_key = 'artefacts' + '/' + 'architecture' + '/' + 'variational' + '/' + 'arguments.json'
-        self.metadata_ = 'viability/external'
-
-        # Prefix
-        self.prefix = 'warehouse' + '/' + 'variational'
