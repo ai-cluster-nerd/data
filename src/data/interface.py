a7427fa5aeeb20959d9f0ffbfbf611d02e14b70e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-06-10 16:55:58 +0100

    Initial commit

---
+++ src/data/interface.py
@@ -0,0 +1,56 @@
+"""Module interface.py"""
+import logging
+import os
+import pandas as pd
+
+import src.data.codes
+import src.data.reference
+import src.data.menu
+import src.elements.specifications as se
+import src.elements.s3_parameters as s3p
+import src.data.specifications
+
+
+class Interface:
+    """
+    Interface
+    """
+
+    def __init__(self, s3_parameters: s3p.S3Parameters):
+        """
+
+        :param s3_parameters: The overarching S3 (Simple Storage Service) parameters
+                              settings of this project, e.g., region code name, buckets, etc.
+        """
+
+        self.__s3_parameters: s3p.S3Parameters = s3_parameters
+
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
+
+    def exc(self) -> list[se.Specifications]:
+        """
+
+        :return:
+        """
+
+        # Codes
+        _codes: list[str] = src.data.codes.Codes().exc()
+        codes = [os.path.split(code)[-1] for code in _codes]
+        self.__logger.info(codes)
+
+        # Reference: of institutions
+        reference: pd.DataFrame = src.data.reference.Reference(
+            s3_parameters=self.__s3_parameters).exc(codes=codes)
+
+        # Menu
+        src.data.menu.Menu().exc(reference=reference)
+
+        # Structure for computations: ref. src.elements.specifications.py
+        specifications_: list[se.Specifications] = src.data.specifications.Specifications().exc(
+            reference=reference)
+
+        return specifications_
---
+++ src/predictions/structures.py
@@ -0,0 +1,68 @@
+"""Module structures.py"""
+import pandas as pd
+
+import src.elements.master as mr
+import src.elements.structures as st
+
+
+class Structures:
+    """
+    Builds the training, testing, and futures data structures
+    """
+
+    def __init__(self, master: mr.Master, arguments: dict):
+        """
+
+        :param master:
+        :param arguments:
+        """
+
+        self.__master = master
+        self.__arguments = arguments
+
+        # Estimates
+        estimates = master.estimates
+        estimates['date'] = pd.to_datetime(estimates['timestamp'], unit='us')
+        estimates.drop(columns='timestamp', inplace=True)
+        self.__estimates = estimates.sort_values(by='date', ascending=True, inplace=False)
+
+    def __get_structures(self, section: str) -> pd.DataFrame:
+        """
+        
+        :param section: training or testing
+        :return: 
+        """
+
+        match section:
+            case 'training':
+                instances = self.__master.training.copy()
+            case 'testing':
+                instances = self.__master.testing.copy()
+            case _:
+                raise ValueError(f'Invalid Section: {section}')
+
+        instances['date'] = pd.to_datetime(instances['week_ending_date'], format='%Y-%m-%d')
+        instances.drop(columns='week_ending_date', inplace=True)
+
+        data = instances[['date']].merge(self.__estimates, how='left', on='date')
+
+        return data
+
+    def __futures(self) -> pd.DataFrame:
+        """
+
+        :return:
+        """
+
+        return self.__estimates[-self.__arguments.get('ahead'):]
+
+    def exc(self) -> st.Structures:
+        """
+
+        :return:
+        """
+
+        return st.Structures(
+            training=self.__get_structures(section='training'),
+            testing=self.__get_structures(section='testing'),
+            futures=self.__futures())
